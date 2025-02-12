fc8c373c5472a1ea59d4e0c862828f94f6712144
songjuseok <realsongzi93@gmail.com>
2025-02-12 17:38:25 +0900

    build.gradle.kts version 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "kr.co.lion.algorithm"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "kr.co.lion.algorithm"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
